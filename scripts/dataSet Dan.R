578e8a5caaf69c20077bd7d06152c9417129b6ad
Dan Knox <danielknox@hotmail.co.uk>
2019-07-18 12:23:12 +0200

    Pushing Dans data stuff

---
+++ scripts/dataSet Dan.R
@@ -0,0 +1,107 @@
+# Aiden & Abe
+# Description: Cleaning the data
+# Created: 07-09-2019
+# Last Updated: 07-09-2019
+
+options(scipen=999)
+library(tidyverse)
+library(lubridate)
+
+df <- read.csv("data/raw/teaching_training_data.csv")
+
+# READ IN DATA
+
+# now read in data for each assessment
+# test for cognitive fluency
+df_cft <- read.csv("data/raw/teaching_training_data_cft.csv")
+# communication ability
+df_com <- read.csv("data/raw/teaching_training_data_com.csv")
+# grit
+df_grit <- read.csv("data/raw/teaching_training_data_grit.csv")
+# numeracy
+df_num <- read.csv("data/raw/teaching_training_data_num.csv")
+# optimism
+df_opt <- read.csv("data/raw/teaching_training_data_opt.csv")
+
+# each individual should only have one assessment
+# set up the data so this is the case...
+# also need to only keep the unid and score
+df_cft <- df_cft %>% 
+  select(unid, cft_score) %>% 
+  distinct(unid, .keep_all = TRUE)
+
+# we want to do this for all 5 asssessments
+# keep_all keeps the rest of the varables in the data frame instead of just the unid, in this case, keep the scores as well
+helper_function <- function(file_name) {
+  file_name %>% 
+    select(2:3) %>% 
+    distinct(unid, .keep_all = TRUE)
+}
+
+df_com <- helper_function(df_com)
+df_grit <- helper_function(df_grit)
+df_num <- helper_function(df_num)
+df_opt <- helper_function(df_opt)
+
+
+df_assess <- full_join(df_cft, df_com, by ="unid")
+df_assess <- df_assess %>% 
+  full_join(df_grit, by ="unid") %>% 
+  full_join(df_num, by ="unid") %>% 
+  full_join(df_opt, by ="unid")
+
+df <- full_join(df, df_assess, by ="unid")
+
+rm(df_assess, df_cft, df_com, df_grit, df_num, df_opt)
+
+#Remove repeating survey_num
+df <- df %>% distinct(unid, .keep_all = TRUE)
+df <- df %>% 
+  mutate(age_at_survey = (interval(dob, survey_date_month)/years(1))-0.333) %>% 
+  mutate(age = floor(age_at_survey) )
+
+df$survey_date_month <- as.Date(df$survey_date_month)
+
+df$survey_date_month <- df$survey_date_month %m-% months(4)
+
+df$survey_date_month <- as.numeric(df$survey_date_month)
+
+#Removing post-first survey columns
+df <- subset(df, select = -c(X,survey_num,job_start_date,job_leave_date,company_size,monthly_pay))
+
+#Conor's imputation code
+
+(colSums(is.na(df))*100)/dim(df)[1]
+df <- subset(df, select = -c(peoplelive_15plus, num_score, province, numearnincome, com_score, age_at_survey, dob))
+
+df$age_sqrd <- (df$age)^2
+
+glm=glm(working ~ ., family = binomial("logit"), data = na.omit(df))
+summary(glm)
+glmstep=step(glm)
+summary(glmstep)
+
+glmnull=glm(working ~ 1, family = binomial("logit"), data = na.omit(df))
+glmnull
+
+cost.misc <- function(response, pred) mean(abs(response-pred) > 0.5)
+1-cost.misc(df_omit$working, predict(glmstep,data=df_omit,type="response"))
+1-cost.misc(df_omit$working, predict(glmnull,data=df_omit,type="response"))
+
+predict(glmstep,data=df_omit,type="response")
+
+max(predict(glmstep,data=df_omit,type="response"))
+
+#preProcess_missingdata_model <- preProcess(heart_mv, method='knnImpute') 
+#preProcess_missingdata_model 
+#heart_2 <- predict(preProcess_missingdata_model, newdata = heart_mv)
+
+#Practice decision tree
+
+#Practice linear regression
+
+###############################################################################################################
+# Everyone
+# Description: Count NAs in each column
+# Created: 07-18-2019
+# Last Updated: 07-18-2019
